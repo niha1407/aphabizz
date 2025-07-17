d32f01b9f3b5747a570a2ecce5526ad1951ea164
niha1407 <127158800+niha1407@users.noreply.github.com>
2025-07-18 02:46:56 +0530

    Update Logo.js

--- src/Logo.js
+++ src/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const LogoSVG = () => (
   <div className="logo-wrapper">
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="120.000000pt" height="70.000000pt" viewBox="0 0 281.000000 94.000000" preserveAspectRatio="xMidYMid meet">
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="200.000000pt" height="70.000000pt" viewBox="0 0 281.000000 94.000000" preserveAspectRatio="xMidYMid meet">
 <metadata>
 Created by potrace 1.10, written by Peter Selinger 2001-2011
 </metadata>
